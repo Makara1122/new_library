7a082c696f8e43f8840606b9d84cfcc15ed077f4
Makara <148768781+Makara1122@users.noreply.github.com>
2024-09-18 09:37:40 +0700

    Update loadlinux.groovy

--- vars/loadlinux.groovy
+++ vars/loadlinux.groovy
@@ -1,4 +1,8 @@
 def call(Map config = [:]){
+  if(!config.name){
+    error "No script name provided config !"
+  }
+  
   def loadresource = libraryResource "com/planetpop/script/linux/${config.name}"
   writeFile file: "${config.name}", text: loadresource
   sh " chmod +x ./${config.name} "
